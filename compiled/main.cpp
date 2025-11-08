8883fc73751c06ef3c2f90665814c62b468878e7
Yesid M <wujibifan.hh@gmail.com>
2025-11-08 12:50:10 -0500

    add menu loop structure with switch-case options

--- compiled/main.cpp
+++ compiled/main.cpp
@@ -8,6 +8,57 @@ struct Node{
 };
 
 int main(int argc, char** argv) {
-    std::cout << "Hello world!";
+    int option = 0;
+    
+    do{
+    	std::cout << " -- MAIN OPTIONS -- " << endl;
+        std::cout << "1. Tree Weight" << endl;
+        std::cout << "2. Create tree" << endl;
+		std::cout << "3. Print Tree" << endl;
+        std::cout << "4. Pre-order" << endl;
+        std::cout << "5. In-order" << endl;
+        std::cout << "6. Post-order" << endl;
+        std::cout << "7. Print tree properties" << endl;
+        std::cout << "8. Print leaf nodes" << endl;
+        std::cout << "9. Delete tree" << endl;
+        std::cout << "10. Close application" << endl;
+    	std::cout << "\n" << endl;
+        std::cout << " - Select a option: ";
+        std::cin >> option;
+    
+        switch(option){
+            case 1:
+                std::cout << " -- Tree Weight -- " << endl;
+            break;
+            case 2:
+                std::cout << " -- Create tree -- " << endl;
+            break;
+            case 3:
+                std::cout << " -- Print Tree -- " << endl;
+            break;
+            case 4:
+            	std::cout << " -- Pre-order -- " << endl;
+            break;
+            case 5:
+            	std::cout << " -- In-order -- " << endl;
+            break;
+            case 6:
+            	std::cout << " -- Post-order -- " << endl;
+            break;
+            case 7:
+            	std::cout << " -- Print tree properties -- " << endl;
+            break;
+            case 8:
+            	std::cout << " -- Print leaf nodes -- " << endl;
+            break;
+            case 9:
+            	std::cout << " -- Delete tree -- " << endl;
+            break;
+            case 10:
+                std::cout << " - Finishing program..." << endl;
+            break;
+        }
+        
+    }while(option != 10);
 	return 0;
 }
